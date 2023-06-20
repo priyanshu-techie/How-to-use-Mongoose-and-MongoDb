3b7e5c86e615baf5c65d4cae00ed6ea03caeda02
Priyanshu_Agrawal <priyanshu100agrawal@gmail.com>
2023-06-20 16:08:39 +0530

    revising

--- mongoose2/server.js
+++ mongoose2/server.js
@@ -1,20 +1,27 @@
+const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose')
 
 async function main() {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/temporary');
         console.log("db connected");
+        const kitSonSchema=new mongoose.Schema({
+            name:String,
+            mother:ObjectId,
+            age:Number
+        })
         const kittySchema = new mongoose.Schema({
-            name: String
+            name: String,
+            children:[kitSonSchema]
         });
-        
-        
+
         const Kitten = mongoose.model('Kitten', kittySchema);
+        const Kitbab= mongoose.model('KitBaby',kitSonSchema);
+
+
         
-        const kit1= await Kitten.findOne({name:"priyanshi"});
-        kit1.name='prajapati'
-        kit1.save();
-        console.log(kit1);
+
+
     }
     catch (e) {
         console.log(e);
